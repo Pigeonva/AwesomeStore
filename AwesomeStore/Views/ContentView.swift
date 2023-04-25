1fd6a9b3ca7ffffae9a52adbbd4527756b78fc7a
Pigeonva <i-realman@mail.ru>
2023-04-25 16:03:49 +0300

    implement profile view into content view

--- AwesomeStore/Views/ContentView.swift
+++ AwesomeStore/Views/ContentView.swift
@@ -13,54 +13,59 @@ struct ContentView: View {
     @State var view = "House"
     
     var body: some View {
-        VStack {
-            Spacer()
-            Text("\(view)")
-            Spacer()
-            HStack {
-                Spacer()
-                Button {
-                    self.view = "House"
-                    self.selected = 0
-                } label: {
-                    Image(systemName: "house")
-                        .font(.title2)
-                        .foregroundColor(self.selected == 0 ? .black : .gray)
-                }
-                Spacer()
-                Button {
-                    self.view = "Favourite"
-                    self.selected = 1
-                } label: {
-                    Image(systemName: "heart")
-                        .font(.title2)
-                        .foregroundColor(self.selected == 1 ? .black : .gray)
-                }
-                Spacer()
-                Button {
-                    self.view = "Cart"
-                    self.selected = 2
-                } label: {
-                    Image(systemName: "cart")
-                        .font(.title2)
-                        .foregroundColor(self.selected == 2 ? .black : .gray)
-                }
-                Spacer()
-                Button {
-                    self.view = "Profile"
-                    self.selected = 3
-                } label: {
-                    Image(systemName: "person")
-                        .font(.title2)
-                        .foregroundColor(self.selected == 3 ? .black : .gray)
-                }
-                Spacer()
+        ZStack {
+            if selected == 3 {
+                ProfileView()
             }
-            .padding(.top, 15)
-            .padding(.bottom, 5)
-            .frame(height: 63)
-            .background(Color.yellow, ignoresSafeAreaEdges: .all)
-            .cornerRadius(20)
+            VStack {
+                Spacer()
+                Text("\(view)")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button {
+                        self.view = "House"
+                        self.selected = 0
+                    } label: {
+                        Image(systemName: "house")
+                            .font(.title2)
+                            .foregroundColor(self.selected == 0 ? .black : .gray)
+                    }
+                    Spacer()
+                    Button {
+                        self.view = "Favourite"
+                        self.selected = 1
+                    } label: {
+                        Image(systemName: "heart")
+                            .font(.title2)
+                            .foregroundColor(self.selected == 1 ? .black : .gray)
+                    }
+                    Spacer()
+                    Button {
+                        self.view = "Cart"
+                        self.selected = 2
+                    } label: {
+                        Image(systemName: "cart")
+                            .font(.title2)
+                            .foregroundColor(self.selected == 2 ? .black : .gray)
+                    }
+                    Spacer()
+                    Button {
+                        self.view = "Profile"
+                        self.selected = 3
+                    } label: {
+                        Image(systemName: "person")
+                            .font(.title2)
+                            .foregroundColor(self.selected == 3 ? .black : .gray)
+                    }
+                    Spacer()
+                }
+                .padding(.top, 15)
+                .padding(.bottom, 5)
+                .frame(height: 63)
+                .background(Color.white, ignoresSafeAreaEdges: .all)
+                .cornerRadius(20)
+            }
         }
     }
 }
