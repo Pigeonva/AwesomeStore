e8c73d723068f4b66d11a4876a271153b2669697
Pigeonva <i-realman@mail.ru>
2023-04-25 11:38:51 +0300

    Initial Commit

---
+++ AwesomeStore/AwesomeStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwesomeStoreApp.swift
+//  AwesomeStore
+//
+//  Created by Артур Фомин on 25.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct AwesomeStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
